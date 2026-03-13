e99c0a68fc121efb9e5aaa6340819aae959af42a
FehrDevelopment <terence.fehr@gmail.com>
2026-03-13 09:58:26 -0600

    Use environment variable for server port

--- server.js
+++ server.js
@@ -83,7 +83,9 @@ app.get("*", (req, res) => {
 // --------------------------------------------------
 // 8. Start server
 // --------------------------------------------------
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 
 
